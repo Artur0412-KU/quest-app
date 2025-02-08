5dc44308a26ad04198f30e5dc4b00e13a0335ffa
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-02-08 18:46:43 +0200

    Created Register component

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -1,10 +1,11 @@
 'use client'
-import React, {useState} from 'react';
+import React, {use, useState} from 'react';
 import CloseSvg from '@/public/icons/close.svg'
 import Google from '@/public/icons/google.svg'
 import Facebook from '@/public/icons/image.svg'
 import Image from "next/image";
 import Button from "@/app/components/Button/Button";
+import Input from "@/app/components/Input/Input";
 
 const Login = ({id}: {id: string}) => {
     const [username, setUsername] = useState('');
@@ -30,12 +31,8 @@ const Login = ({id}: {id: string}) => {
                 <div className='flex flex-col gap-[32px]'>
                     <h2 className="font-bold text-lg text-[28px] text-center">Log in</h2>
                     <div className='grid grid-cols-2 gap-[12px]'>
-                        <button className="btn2 btn-active bg-white border-[#dddddd] border rounded-[4px] py-[12px] flex justify-center">
-                            <Image src={Google} alt={'google'} />
-                        </button>
-                        <button className="btn2 btn-active bg-white border-[#dddddd] border rounded-[4px] py-[12px] flex justify-center">
-                            <Image src={Facebook} alt={'facebook'} />
-                        </button>
+                        <Button image={Google} className={'btn2 btn-active bg-white border-[#dddddd] border rounded-[4px] py-[12px] flex justify-center'}/>
+                        <Button image={Facebook} className={'btn2 btn-active bg-white border-[#dddddd] border rounded-[4px] py-[12px] flex justify-center'}/>
                     </div>
                     <div className='flex flex-row items-center justify-center gap-[8px]'>
                         <hr className='bg-[#DDDDDD] w-full'/>
@@ -43,22 +40,8 @@ const Login = ({id}: {id: string}) => {
                         <hr className='bg-[#DDDDDD] w-full'/>
                     </div>
                     <div className='flex flex-col gap-[16px]'>
-                        <div className='flex flex-col gap-[8px]'>
-                            <h3 className='text-[16px] font-semibold'>Username or Email</h3>
-                            <input type="text" placeholder="Type here"
-                                   className="input input-bordered w-full bg-[#F8F8F8]" value={username} onChange={(e) => handleChangeUsername(e)}/>
-                        </div>
-
-                        <div className='flex flex-col gap-[8px]'>
-                            <div className='flex justify-between'>
-                                <h3 className='text-[16px] font-semibold'>Password</h3>
-                                <h3 onClick={() => alert("Temporally, you can't change the password(")} className='text-[16px] font-semibold text-[#228EDF] cursor-pointer'>Forgot</h3>
-                            </div>
-
-                            <input type="password" placeholder=""
-                                   className="input input-bordered w-full bg-[#F8F8F8]" value={password} onChange={(e) => handleChangePassword(e)}/>
-                        </div>
-
+                        <Input name={"Username or Email"} type={"placeholder"} value={username} className={"input input-bordered w-full bg-[#F8F8F8]"} onChange={ handleChangeUsername} />
+                        <Input name={"Password"} type={"password"} value={password} className={"input input-bordered w-full bg-[#F8F8F8]"} onChange={ handleChangePassword} forgotText={'Forgot'}/>
                         <div className='flex flex-col gap-[8px]'>
                             {!username || !password ?
                                 <Button className={"btn2 btn-disabled bg-[#E4572E] rounded-[4px] py-[12px] flex justify-center w-full text-white font-semibold"} text={'Log In'} onClick={() => alert('no')}/>
@@ -67,7 +50,10 @@ const Login = ({id}: {id: string}) => {
                             }
                             <p className='text-center text-[#808080] text-[14px]'>New here? <span
                                 className='text-[#228EDF] cursor-pointer'
-                                onClick={() => alert("Temporally, you can't create account(")}>Create a 404Quest account</span></p>
+                                onClick={()=> {
+                                    (document.getElementById('my_modal_5') as HTMLDialogElement).close(),
+                                    (document.getElementById('my_modal_6') as HTMLDialogElement | null)?.showModal()
+                                }}>Create a 404Quest account</span></p>
                         </div>
 
                     </div>
--- types/types.ts
+++ types/types.ts
@@ -1,7 +1,19 @@
+import React from "react";
+
 type ButtonProps = {
     className?: string;
     onClick?: () => void;
-    text: string;
+    text?: string;
+    image?: string;
 }
 
-export type { ButtonProps };
+type InputProps = {
+    className?: string;
+    name: string;
+    forgotText?: string;
+    type: string;
+    value: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export type { ButtonProps, InputProps };
