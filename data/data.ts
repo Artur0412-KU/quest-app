517d30447dbf6858f6615731a8a5d5b28c9908a0
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-02-10 12:28:36 +0200

    Created quest-roadmap with quest tasks

--- data/data.ts
+++ data/data.ts
@@ -1,6 +1,40 @@
 const tasks = [
-    {id: 1, name: 'Task 1', lat: 50.4501, lng: 30.5234, description: 'Lorem ispum tara pitec'},
-    {id: 2, name: 'Task 2', lat: 50.4519, lng: 30.5255, description: 'Розгадай загадку біля пам\'ятника'}
+    {
+        id: 1,
+        title: 'Task 1',
+        list: [
+            { id: 1, name: 'Subtask 1.1',},
+            { id: 2, name: 'Subtask 1.2', },
+            { id: 3, name: 'Subtask 1.3', }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Task 2',
+        list: [
+            { id: 1, name: 'Subtask 2.1',},
+            { id: 2, name: 'Subtask 2.2',},
+            { id: 3, name: 'Subtask 2.3',}
+        ]
+    },
+    {
+        id: 3,
+        title: 'Task 3',
+        list: [
+            { id: 1, name: 'Subtask 3.1'},
+            { id: 2, name: 'Subtask 3.2'},
+            { id: 3, name: 'Subtask 3.3'}
+        ]
+    },
+    {
+        id: 4,
+        title: 'Task 4',
+        list: [
+            { id: 1, name: 'Subtask 4.1'},
+            { id: 2, name: 'Subtask 4.2'},
+            { id: 3, name: 'Subtask 4.3'}
+        ]
+    },
 ]
 
 export default tasks;
