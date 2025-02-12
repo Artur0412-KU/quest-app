14115cea6255d2ece24630ee8e0438ab6872d7a5
Humen Vasyl <vasyahumenit@gmail.com>
2025-02-12 23:12:21 +0200

    work

--- app/addQuest/action.ts
+++ app/addQuest/action.ts
@@ -38,6 +38,55 @@
 //   if (error) throw new Error(error.message);
 // }
 
+// =====================================================
+
+// import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
+
+// export async function addQuest(
+//   title: string,
+//   description: string,
+//   img: string,
+//   img_bg: string,
+//   type: string,
+//   subtasks: { id: number; name: string }[]
+// ) {
+//   // Огортаємо cookies() у функцію, яка повертає Promise
+//   const cookiesData = async () => {
+//     return cookies();
+//   };
+
+//   // Створюємо клієнт Supabase, передаючи cookies як асинхронну функцію
+//   const supabase = createServerActionClient({ cookies: cookiesData });
+
+//   // Отримуємо дані користувача з Supabase
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser();
+
+//   // Вставляємо новий запис в таблицю "quest"
+//   const { error } = await supabase.from("quest").insert([
+//     {
+//       title,
+//       description,
+//       time: new Date().toISOString(),
+//       author: user?.email ?? "Анонім",
+//       user_id: user?.id || 0,
+//       img,
+//       img_bg,
+//       type,
+//       subtasks,
+//     },
+//   ]);
+
+//   // Якщо сталася помилка при вставці, викидаємо її
+//   if (error) throw new Error(error.message);
+// }
+
+
+
+"use server";
+
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
@@ -49,20 +98,13 @@ export async function addQuest(
   type: string,
   subtasks: { id: number; name: string }[]
 ) {
-  // Огортаємо cookies() у функцію, яка повертає Promise
-  const cookiesData = async () => {
-    return cookies();
-  };
+  const supabase = createServerActionClient({ cookies });
 
-  // Створюємо клієнт Supabase, передаючи cookies як асинхронну функцію
-  const supabase = createServerActionClient({ cookies: cookiesData });
-
-  // Отримуємо дані користувача з Supabase
+  // Отримуємо поточного користувача (якщо є)
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // Вставляємо новий запис в таблицю "quest"
   const { error } = await supabase.from("quest").insert([
     {
       title,
@@ -77,6 +119,5 @@ export async function addQuest(
     },
   ]);
 
-  // Якщо сталася помилка при вставці, викидаємо її
   if (error) throw new Error(error.message);
 }
