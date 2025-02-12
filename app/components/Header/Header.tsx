237d169c5f7399ddfbc4f8f5a19d1e354f6a3cf1
Humen Vasyl <vasyahumenit@gmail.com>
2025-02-12 20:48:45 +0200

    chadfd

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -8,36 +8,7 @@ import Link from "next/link";
 import Button from "@/app/components/Button/Button";
 import BurgerMenuIcon from '@/public/icons/burger-menu.svg'
 import BurgerMenu from "@/app/components/BurgerMenu/BurgerMenu";
-<<<<<<< HEAD
-import {usePathname} from "next/navigation";
 
-const Header = () => {
-    const pathname = usePathname()
-    return (
-        <div className='navbar bg-base-100 py-[20px] px-[80px] flex flex-row justify-between bg-white max-lg:px-[32px]'>
-            <div className='flex items-center justify-between gap-[48px]'>
-                <Image src={Logo} alt={'logo'} />
-                <div className='flex gap-[16px] max-lg:hidden'>
-                        <Link href={'/'} className={`font-medium [&.active]:text-[#33818A] [&.active]:font-extrabold hover:text-[#33818A] hover:font-extrabold ${pathname === '/' ? 'active' : ''}`}>Main page</Link>
-                        <Link href={'/quests'} className={`font-medium [&.active]:text-[#33818A] [&.active]:font-extrabold hover:text-[#33818A] hover:font-extrabold ${pathname === '/quests' ? 'active' : ''}`}>All quests</Link>
-                        <Link href={'/questions'} className={`font-medium [&.active]:text-[#33818A] [&.active]:font-extrabold hover:text-[#33818A] hover:font-extrabold ${pathname === '/questions' ? 'active' : ''}`}>FAQ</Link>
-                </div>
-            </div>
-
-            <div className='flex gap-[16px] max-lg:hidden'>
-                <Button className={"btn py-[16px] px-[28px] font-semibold bg-white text-[#E4572E] border-[#E4572E] border-[2px] hover:border-orange-500 hover:bg-white hover:text-orange-500 "} onClick={() => alert('Creating a quest will be soon ^)')} text={'Create a quest'}/>
-                <Button className={"btn py-[16px] px-[28px] font-semibold bg-[#E4572E] text-white hover:bg-orange-500 border-none"} onClick={() => {(document.getElementById('my_modal_5') as HTMLDialogElement | null)?.showModal()}} text={'Log In'}/>
-            </div>
-
-
-            <Login id={"my_modal_5"}/>
-            <Register id={"my_modal_6"}/>
-
-            <BurgerMenu/>
-
-        </div>
-    );
-=======
 import { createClient } from "@/src/utils/supabase/client";
 import { logout } from "@/app/logaut/actions";
 
@@ -169,7 +140,6 @@ const Header = () => {
       <Register id={"my_modal_6"} />
     </div>
   );
->>>>>>> vasya
 };
 
 export default Header;
